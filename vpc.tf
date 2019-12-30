1bb9f7face30f5c8702c9e68883b6e6e7f1bf821
suprajagantena <suprajagantena@gmail.com>
2019-12-30 16:26:26 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -15,18 +15,18 @@ resource "aws_vpc" "dev" {
     default = "10.0.0.0/16"
   }
 }
-  resource "aws_subnet" "dev" {
-  vpc_id     = "${aws_vpc.dev.id}"
-  cidr_block = "default"
+variable "private_subnet_cidr" {
+    description = "CIDR for the Public Subnet"
+    default = "10.0.1.0/16"
 
   tags = {
     Name = "dev-pri-1"
   }
 }
   
- resource "aws_subnet" "dev" {
-  vpc_id     = "${aws_vpc.dev.id}"
-  cidr_block = "default"
+variable "private_subnet_cidr" {
+    description = "CIDR for the Public Subnet"
+    default = "10.0.2.0/16"
 
   tags = {
     Name = "dev-pri-2"
@@ -47,18 +47,18 @@ resource "aws_vpc" "qa" {
     description = "CIDR for the Public Subnet"
     default = "10.0.0.0/18"
    
-  resource "aws_subnet" "qa-pri-1" {
-  vpc_id     = "${aws_vpc.qa.id}"
-  cidr_block = "10.0.2.0/18"
+ variable "private_subnet_cidr" {
+    description = "CIDR for the Public Subnet"
+    default = "10.0.1.0/18
 
   tags = {
     Name = "qa-pri-1"
   }
 }
   
-  resource "aws_subnet" "qa-pri-2" {
-  vpc_id     = "${aws_vpc.qa.id}"
-  cidr_block = "10.0.3.0/18"
+ variable "private_subnet_cidr" {
+    description = "CIDR for the Public Subnet"
+    default = "10.0.2.0/18"
 
   tags = {
     Name = "qa-pri-2"
@@ -77,18 +77,18 @@ resource "aws_vpc" "qa" {
     description = "CIDR for the Public Subnet"
     default = "10.0.0.0/24"
   
-  resource "aws_subnet" "prd-pri-1" {
-  vpc_id     = "${aws_vpc.prd.id}"
-  cidr_block = "10.0.2.0/24"
+  variable "private_subnet_cidr" {
+    description = "CIDR for the Public Subnet"
+    default = "10.0.1.0/24"
 
   tags = {
     Name = "prd-pri-1"
   }
 }
   
-  resource "aws_subnet" "prd-pri-2" {
-  vpc_id     = "${aws_vpc.prd.id}"
-  cidr_block = "10.0.3.0/24"
+  variable "private_subnet_cidr" {
+    description = "CIDR for the Public Subnet"
+    default = "10.0.2.0/24"
 
   tags = {
     Name = "prd-pri-2"
