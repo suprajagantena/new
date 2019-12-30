517afe1361302e96c06660f7d0f285542ff60a6d
suprajagantena <suprajagantena@gmail.com>
2019-12-30 12:33:12 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  version = "~> 2.0"
+  access_key = "AKIATOUVDBKIM2ZSZ2QS"
+ secret_key = "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"
+  region  = "us-east-1"
+}
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "dedicated"
