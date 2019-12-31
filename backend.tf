54cd215035cb824f37176d4fe03c53a774155ec6
suprajagantena <suprajagantena@gmail.com>
2019-12-30 23:25:30 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
 ## State Backend
-terraform {
-  backend "s3" {
-    bucket  = "mybucket12345@123"
+#terraform {
+ # backend "s3" {
+ #   bucket  = "mybucket12345@123"
  #   key     = "aws/${var.project}/${var.environment}"
-    region  = "us-east-1"
-    profile = "default"
-    encrypt = "true"
+ #  region  = "us-east-1"
+ #  profile = "default"
+ #  encrypt = "true"
   #  lock_table = "terraform"
   }
 }
