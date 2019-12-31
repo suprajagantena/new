7798b9c5daa5afafc75abfb35c943f5cb2a9af00
suprajagantena <suprajagantena@gmail.com>
2019-12-31 12:35:39 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -73,14 +73,14 @@ resource "aws_subnet" "prd-subnet-public-1" {
   
 }
 
-resource "aws_subnet" "prd-subnet-private-1"
+resource "aws_subnet" "prd-subnet-private-1"{
     vpc_id = "aws_vpc.prd.id"
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true" 
   
 }
 
-resource "aws_subnet" "prd-subnet-private-2"
+resource "aws_subnet" "prd-subnet-private-2"{
     vpc_id = "aws_vpc.prd.id"
     cidr_block = "10.0.3.0/24"
     map_public_ip_on_launch = "true" 
