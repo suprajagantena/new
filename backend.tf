40bd5a4e1f1b4ef075e0c24cd410466eda1e9378
suprajagantena <suprajagantena@gmail.com>
2019-12-30 23:04:19 -0500

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,11 @@
+## State Backend
+terraform {
+  backend "s3" {
+    bucket  = "mybucket12345@123"
+ #   key     = "aws/${var.project}/${var.environment}"
+    region  = "us-central-1"
+    profile = "default"
+    encrypt = "true"
+    lock_table = "terraform"
+  }
+}
