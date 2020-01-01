1095d7d4d6e89b47cb0de316db573d815f9744af
suprajagantena <suprajagantena@gmail.com>
2020-01-01 18:15:07 -0500

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,16 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = "aws_vpc.dev.id"
+  }
+  
+  output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = "aws_vpc.qa.id"
+  }
+  
+  output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = "aws_vpc.prd.id"
+  }
+  
+  
