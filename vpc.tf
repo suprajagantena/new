84d9c8449fd0c8252cb5f4ca026ca678e8e418f8
suprajagantena <suprajagantena@gmail.com>
2020-01-01 21:23:25 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -17,7 +17,7 @@ resource "aws_vpc" "dev" {
 resource "aws_instance" "web" {
   ami           = "ami-00068cd7555f543d5"
   instance_type = "t2.micro"
-   aws_subnet = "aws_subnet.dev-subnet-public-1.id"
+  subnet = "aws_subnet.dev-subnet-public-1.id"
   tags = {
     Name = "instance_dev_pub"
   }
