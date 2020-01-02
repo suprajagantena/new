4d41223874784296d659a197c33ad4050a359dd1
suprajagantena <suprajagantena@gmail.com>
2020-01-01 21:21:27 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
-output "vpc_id" {
+output "vpc_id_dev" {
   description = "The ID of the VPC"
   value       = "aws_vpc.dev.id"
   }
   
-  output "vpc_id" {
+  output "vpc_id_qa" {
   description = "The ID of the VPC"
   value       = "aws_vpc.qa.id"
   }
   
-  output "vpc_id" {
+  output "vpc_id_prd" {
   description = "The ID of the VPC"
   value       = "aws_vpc.prd.id"
   }
