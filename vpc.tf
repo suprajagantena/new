29abe17a016cc2fb2ad5041772e10ddf211fc51a
suprajagantena <suprajagantena@gmail.com>
2020-01-10 18:21:58 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -11,7 +11,6 @@ resource "aws_vpc" "dev" {
     vpc_id = "aws_vpc.dev.id"
     cidr_block = "10.0.1.0/16"
     map_public_ip_on_launch = "true" 
-   depends_on = [aws_vpc.dev]
    tags = {
     Name = "dev-pub"
   }
