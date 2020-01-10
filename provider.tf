268c07d53246d869be53b97832774ff28b6458df
suprajagantena <suprajagantena@gmail.com>
2020-01-10 16:23:07 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
    #version = "~> 2.43"
-   #region  = "us-east-1"
+    region  = "us-east-1"
     assume_role {
     role_arn     = "arn:aws:iam::546366803473:role/jenkins"
     session_name = "TERRAFORM"
